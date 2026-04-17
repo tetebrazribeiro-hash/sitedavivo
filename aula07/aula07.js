f6c60732e0a45eb27654405e6bc7699e217f94c9
tetebrazribeiro-hash <tetebrazribeiro@gmail.com>
2026-04-16 21:11:42 -0300

    concluido

---
+++ aula07/aula07.js
@@ -0,0 +1,9 @@
+//OPERADORES DE COMPARAÇÃO
+let idade = 18;
+
+console.log(idade == '18'); //true
+console.log(idade === "18"); //false
+console.log(idade > 10);//true
+console.log(idade < 8); //false
+console.log(idade >= 35); //false
+console.log(idade <= 20); //true
